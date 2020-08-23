d08269112f55bfdac4fbaf69b0d9e0bed418a742
VinayakMarraiya <vinayak.marraiya@gmail.com>
2020-08-23 11:25:45 +0530

    My Git Project

---
+++ MyGitProject/src/com/vinayak/MyCalc.java
@@ -0,0 +1,12 @@
+package com.vinayak;
+
+public class MyCalc {
+	
+	public int display(int a,int b) {
+		return a+b;
+	}
+
+	public int display(int a,int b,int c) {
+		return a+b+c;
+	}
+}
---
+++ MyGitProject/src/com/vinayak/MyGitClass.java
@@ -0,0 +1,12 @@
+package com.vinayak;
+
+public class MyGitClass {
+
+	public static void main(String[] args) {
+		MyCalc c = new MyCalc();
+		System.out.println(c.display(2, 4));
+		System.out.println(c.display(2, 4, 6));
+
+	}
+
+}
